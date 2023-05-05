b98711e987bc82b775449f2082a03cbe8390901f
Tovci Tovcii <121406294+Tovcii@users.noreply.github.com>
2023-05-05 16:00:17 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import ProfileContainer from "./profilecontainer/ProfileContainer";
+import Footer from "./footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <ProfileContainer />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
